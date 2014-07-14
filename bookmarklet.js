82d868b635346d956389c4857f42e6ad2652f5d2
Jan Bölsche <regular.gonzales@googlemail.com>
2014-07-14 18:53:53 +0200

    bookmarklet works

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,8 @@
 // we expect READING_LIST_DATA to be defined
 var render = require('./render');
-console.log(render(READING_LIST_DATA));
+var html = render(READING_LIST_DATA);
+var container = document.createElement('div');
+container.innerHTML = html;
+container.setAttribute('style', "top:0px;left:0px;z-index:30000;position:fixed;width:300px;background-color:white;border-right: 2px solid 444;");
+document.body.appendChild(container);
+document.body.style.marginLeft = '300px';
