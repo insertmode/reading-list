2c50ab1228d81e1e775aafbd9e8494ad5a65f79d
Jan Bölsche <regular.gonzales@googlemail.com>
2014-07-15 13:27:09 +0200

    rolling back cross-domain iframe, but using an iframe now to prevent CSS leakage

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,8 +1,10 @@
 // we expect READING_LIST_DATA to be defined
 var render = require('./render');
 var html = render(READING_LIST_DATA);
-var container = document.createElement('div');
-container.innerHTML = html;
-container.setAttribute('style', "top:0px;left:0px;z-index:30000;position:fixed;width:300px;background-color:white;border-right: 2px solid 444;");
+console.log(html);
+
+var container = document.createElement('iframe');
+container.setAttribute('style', "top:0px;left:0px;z-index:30000;position:fixed;width:300px;height:100%;background-color:white;border-right: 2px solid 444;");
 document.body.appendChild(container);
+container.contentDocument.body.innerHTML = html;
 document.body.style.marginLeft = '300px';
